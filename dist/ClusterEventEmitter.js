128192d998d27f785fdefeea2156ed1ecd7122d2
DavidUhlíř <posta@daviduhlir.cz>
2024-07-08 20:04:44 +0200

    fixed versions

--- dist/ClusterEventEmitter.js
+++ dist/ClusterEventEmitter.js
@@ -95,4 +95,3 @@ class ClusterEventEmitter extends events_1.default {
 }
 exports.ClusterEventEmitter = ClusterEventEmitter;
 ClusterEventEmitter.initialized = false;
-//# sourceMappingURL=ClusterEventEmitter.js.map
